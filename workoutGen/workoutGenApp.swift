7fff2973a6db923d42c5f881be6f04ecbf9a6775
Nhyira <nhyiratnkansah@gmail.com>
2023-08-16 10:24:52 -0400

    Initial Commit

---
+++ workoutGen/workoutGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  workoutGenApp.swift
+//  workoutGen
+//
+//  Created by scholar on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct workoutGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
