6bf8d18bb089e3e245cc313c6846e15d7bd3bdb8
Nhyira <nhyiratnkansah@gmail.com>
2023-08-16 14:36:47 -0400

    updated

--- workoutGen/ContentView.swift
+++ workoutGen/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textWorkout = ""
+    let workouts = ["Lunges", "Go for a run", "Burpees", "Goblin Squats", "Sit Ups", "Forearm Plank", "Wall sit", "Jumping Jacks","Side Plank", "Leg Raises", "15 Minute Yoga Session", "Russian Twists"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Excercise Log")
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.leading)
+            Spacer ()
+                .frame(height: 400.0)
+            
+            Text("Workout Generator")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Button("Generate") {
+                func pickWorkout() -> String {
+                    let random = Int.random(in: 0..<11)
+                    let workout = workouts[random]
+                    //var random = workout[random]                    var workout = workout.randomElement()!
+                  return workout
+                }
+
+                textWorkout = pickWorkout()
+                //print(randomWorkout)
+            }
+            .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+            Spacer()
+                .frame(height: 68.001)
+            
+            Text(textWorkout)
+                .font(.title)
+            
+                
+            Spacer ()
+            
         }
-        .padding()
     }
 }
 
